f59a0fcf141eb49b45ed88d15bcf1e98185be47c
DenisKantic <denis.kantic18@gmail.com>
2021-11-13 20:06:54 -0600

    created simple html structure with name and className

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>123</p>
+    <div className="container">
+
+      <div className="keypad">
+        <button className="highlight" >Clear</button>
+        <button className="highlight" >C</button>
+        <button className="highlight" >&divide;</button>
+        <button name="7" >7</button>
+        <button name="8" >8</button>
+        <button name="9" >9</button>
+        <button className="highlight" name="*" >&times;</button>
+        <button name="4" >4</button>
+        <button name="5" >5</button>
+        <button name="6" >6</button>
+        <button className="highlight" name="-" >&ndash;</button>
+        <button name="1" >1</button>
+        <button name="2" >2</button>
+        <button name="3" >3</button>
+        <button className="highlight" name="+" >+</button>
+        <button name="0" >0</button>
+        <button className="highlight" name="." >.</button>
+        <button className="highlight" name="=" >=</button>
+      </div>
     </div>
   );
 }
